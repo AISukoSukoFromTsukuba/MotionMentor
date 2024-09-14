469f420e6f9ee93058833687af7d39074bbc1abb
shosei <0426193@gmail.com>
2024-09-14 17:19:55 +0900

    page1 done

--- app.py
+++ app.py
@@ -2,17 +2,34 @@ import streamlit as st
 from components import coach
 
 
+def main_page():
+    st.markdown("## MotionMentor")
+    st.write("あなたの筋トレをサポートします。")
+    st.session_state.train_menu = st.selectbox("種目", ["スクワット", "腕立て伏せ"])
+
+    # 回数をテキストボックスで入力
+    st.session_state.reps = st.text_input("回数", 10)
+
+    # コーチの選択
+    st.session_state.coach = st.selectbox("コーチ", ["ジョージ", "JK", "メスガキ"])
+
+    # トレーニング開始ボタン
+    if st.button("トレーニング開始"):
+        # 全ての入力が完了しているか確認
+        if st.session_state.train_menu and st.session_state.reps and st.session_state.coach:
+            st.session_state.page = "coach"
+            st.rerun()
+        else:
+            st.error("全ての項目を入力してください。")
+
+
 def main():
     if "page" not in st.session_state:
         st.session_state.page = "main"
 
     # main page
     if st.session_state.page == "main":
-        st.title("Hello World!")
-        st.write("Welcome to Streamlit!")
-        if st.button("next page"):
-            st.session_state.page = "coach"
-            st.rerun()
+        main_page()
     elif st.session_state.page == "coach":
         coach.coach_page()
 
