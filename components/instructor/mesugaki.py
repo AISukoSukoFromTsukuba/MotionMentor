5b0b91167886e9dbcee671eb76a92b31cc0c6de6
shun <07shu160@gmail.com>
2024-09-15 22:45:45 +0900

    change key env

--- components/instructor/mesugaki.py
+++ components/instructor/mesugaki.py
@@ -9,7 +9,7 @@ load_dotenv(override=True)
 
 openai.api_type = "azure"
 openai.api_version = "2023-05-15"
-openai.api_base = "https://project-nomad-service.openai.azure.com/"
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_key = os.getenv("AZURE_GPT_KEY")
 
 
